66d122925b768df647c28ba7fcf0f6f10d4e77d2
anaria2040 <75677822+anaria2040@users.noreply.github.com>
2025-06-19 16:00:13 -0700

    main method in Main, default manager is FileBackedTaskManager

--- test/main/manager/FileBackedTaskManagerTest.java
+++ test/main/manager/FileBackedTaskManagerTest.java
@@ -6,29 +6,15 @@ import main.task.Task;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileBackedTaskManagerTest {
-    private File createTempFileWithLines(List<String> data) throws IOException {
-        File tempFile = File.createTempFile("start", ".txt");
-        tempFile.deleteOnExit();
-        try (FileWriter writer = new FileWriter(tempFile, StandardCharsets.UTF_8)) {
-            writer.write("id,type,name,status,description,epic\n");
-            for (String datum : data) {
-                writer.write(datum + "\n");
-            }
-        }
-        return tempFile;
-    }
-
-    private FileBackedTaskManager managerWithSampleTasks() throws IOException {
-        FileBackedTaskManager manager = new FileBackedTaskManager(File.createTempFile("Text", ".txt"));
+    private FileBackedTaskManager managerWithSampleTasks(File file) throws IOException {
+        FileBackedTaskManager manager = new FileBackedTaskManager(file);
         Task task = new Task("task1", "desc task 1");
         manager.addTask(task);
         Epic epic = new Epic("epic1", "epic1");
@@ -40,7 +26,8 @@ class FileBackedTaskManagerTest {
 
     @Test
     public void loadFromFileWhenEmpty() throws IOException {
-        File tempFile = createTempFileWithLines(new ArrayList<>());
+        File tempFile = File.createTempFile("text", ".txt");
+        tempFile.deleteOnExit();
         FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(tempFile);
         fileBackedTaskManager.getAllEpic();
         assertEquals(new ArrayList<>(), fileBackedTaskManager.getAllTasks());
@@ -48,32 +35,24 @@ class FileBackedTaskManagerTest {
 
     @Test
     public void loadFromFileAllTypesOfTasks() throws IOException {
-        List<String> expected = List.of(
-                "1,TASK,Task1,DONE,Description task1 from downloaded",
-                "2,EPIC,Epic2,DONE,Description epic2 downloaded",
-                "3,SUBTASK,Subtask3,DONE,Description subtask3,2"
-        );
-        File tempFile = createTempFileWithLines(expected);
+        File tempFile = File.createTempFile("start", ".txt");
+        tempFile.deleteOnExit();
+        FileBackedTaskManager manager = managerWithSampleTasks(tempFile);
+
         FileBackedTaskManager managerFromFile = FileBackedTaskManager.loadFromFile(tempFile);
-        managerFromFile.getAllTasks();
-        FileBackedTaskManager managerExpected = managerWithSampleTasks();
-        assertEquals(managerExpected.getListTasks(), managerFromFile.getListTasks());
+        assertEquals(manager.getListTasks(), managerFromFile.getListTasks());
     }
 
     @Test
     public void loadFromFileAllTypesOfTasksWithCorrectIds() throws IOException {
-        List<String> given = List.of(
-                "12,TASK,Task1,DONE,Description task1 from downloaded",
-                "22,EPIC,Epic2,DONE,Description epic2 downloaded",
-                "32,SUBTASK,Subtask3,DONE,Description subtask3,22"
-        );
-        File tempFile = createTempFileWithLines(given);
-
+        File tempFile = File.createTempFile("start", ".txt");
+        tempFile.deleteOnExit();
+        FileBackedTaskManager manager = managerWithSampleTasks(tempFile);
+        manager.deleteById(1);
+        manager.deleteById(3);
         FileBackedTaskManager managerFromFile = FileBackedTaskManager.loadFromFile(tempFile);
-        managerFromFile.getAllTasks();
-
-        FileBackedTaskManager fileBackedTaskManagerExpected = managerWithSampleTasks();
-        assertEquals(fileBackedTaskManagerExpected.getListTasks(), managerFromFile.getListTasks());
+        manager.findById(2).setId(1);
+        assertEquals(manager.getListTasks(), managerFromFile.getListTasks());
     }
 
     @Test
