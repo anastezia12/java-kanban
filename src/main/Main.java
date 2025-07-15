2e4976e5292655591ed7ea3ee1e87cd0b78bb182
anaria2040 <75677822+anaria2040@users.noreply.github.com>
2025-07-15 01:41:03 -0700

    Unused import - main.task.Status. deleted

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import main.WebServices.HttpTaskServer;
 import main.manager.Managers;
 import main.manager.TaskManager;
 import main.task.Epic;
-import main.task.Status;
 import main.task.Subtask;
 import main.task.Task;
 
@@ -19,13 +18,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         TaskManager taskManager = Managers.getDefault();
-     Task task1 = new Task("name", "description", LocalDateTime.of(2025, 9, 28, 12, 0), Duration.ofMinutes(30));
-         Epic epic1 = new Epic("name", "description");
-       taskManager.addTask(task1);
-       taskManager.addTask(epic1);
-         Subtask subtask1 = new Subtask("subtask", "desc", epic1, LocalDateTime.of(2025, 9, 28, 11, 0), Duration.ofMinutes(30));
+        Task task1 = new Task("name", "description", LocalDateTime.of(2025, 9, 28, 12, 0), Duration.ofMinutes(30));
+        Epic epic1 = new Epic("name", "description");
+        taskManager.addTask(task1);
+        taskManager.addTask(epic1);
+        Subtask subtask1 = new Subtask("subtask", "desc", epic1, LocalDateTime.of(2025, 9, 28, 11, 0), Duration.ofMinutes(30));
         taskManager.addTask(subtask1);
-         HttpTaskServer server = new HttpTaskServer(taskManager);
+        HttpTaskServer server = new HttpTaskServer(taskManager);
         server.start();
     }
 }
